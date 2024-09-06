00485cb4812a75b0d59ba405d51dae9b9efa978d
Jangwon0319 <ryujw0319@gmail.com>
2024-09-07 00:48:53 +0900

    Initial commit

---
+++ src/main/java/com/example/ssuitfestival/SsuitfestivalApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssuitfestival;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsuitfestivalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsuitfestivalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ssuitfestival/SsuitfestivalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssuitfestival;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsuitfestivalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
