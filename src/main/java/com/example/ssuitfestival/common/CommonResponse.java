880f1d01716d0a673d48aa97f4efdb4079654904
rudeore-098 <rudeore0928@gmail.com>
2024-09-21 16:17:06 +0900

    feat(#11) : CommonResponse 구현

---
+++ src/main/java/com/example/ssuitfestival/common/CommonResponse.java
@@ -0,0 +1,16 @@
+package com.example.ssuitfestival.common;
+
+import lombok.Data;
+
+@Data
+public class CommonResponse<T> {
+    private String statusCode;
+    private T data;
+    private String message;
+
+    public CommonResponse(String statusCode, T data, String message){
+        this.statusCode = statusCode;
+        this.data = data;
+        this.message = message;
+    }
+}
